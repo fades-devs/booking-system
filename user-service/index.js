18ad449001aa90d85f2f44a5b5a098bb924b89a2
Fadoua Assoufi <fadouaassoufi@Fadouas-MacBook-Air.local>
2026-02-10 15:47:17 +0000

    Configured Auth0 for user authentication

--- user-service/index.js
+++ user-service/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const app = express();
 
@@ -15,6 +16,18 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((err) => {
     console.error('Database connection error', err);
     process.exit(1); // stop app if db fails
+});
+
+// user authentication
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: 'RS256'
+});
+// enforce on all endpoints
+app.use(jwtCheck);
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
 });
 
 // routes
