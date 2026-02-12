ae88156c5fb54bb403f41d9c0a3bb3f78b500edc
fades <fades.devs@gmail.com>
2026-02-12 19:32:35 +0000

    Define room service schema model

---
+++ room-service/Room.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    basePrice: {
+        type: Double,
+        required: true
+    },
+    location: {
+        type: String,
+    },
+    partnerId: {
+        type: String,
+        required: true
+    },
+    pictures: [
+        {
+            type: String
+        }
+    ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Room', roomSchema)
