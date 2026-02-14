c4fdeac96e1b689215f3556c40f1e2959956904f
fades <fades.devs@gmail.com>
2026-02-14 16:36:26 +0000

    Implemented sync API for user registration or login

--- user-service/index.js
+++ user-service/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const { auth } = require('express-oauth2-jwt-bearer');
+const User = require('./User');
+
 
 const app = express();
+
+app.use(express.json());
+
 
 // load environment variables
 dotenv.config();
@@ -18,16 +23,68 @@ mongoose.connect(process.env.MONGO_URI)
     process.exit(1); // stop app if db fails
 });
 
+
 // user authentication
-const jwtCheck = auth({
+const checkJwt = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: 'RS256'
 });
+
 // enforce on all endpoints
-app.use(jwtCheck);
+// app.use(jwtCheck);
+
+// add health check ?
+
+// Public route - no authentication required
+app.get('/api/public', (req, res) => {
+  res.json({
+    message: 'Hello from a public endpoint! No authentication required.',
+  });
+});
+
+// Protected route - requires valid JWT
+app.get('/api/private', checkJwt, (req, res) => {
+  res.json({
+    message: 'Hello from a private endpoint!',
+    user: req.auth.payload.sub,
+  });
+});
+
 app.get('/authorized', function (req, res) {
     res.send('Secured Resource');
+});
+
+// route to sync user (register or login)
+app.post('/api/v1/auth/sync', checkJwt, async (req, res) => {
+    try {
+
+        // get the auth0 id from the request token (will be passed by frontend)
+        const auth0Id = req.auth.payload.sub;
+
+        // MORE OPTIMISED - RETURN EARLY -> APPLY THIS LOGIC TO ALL API
+        // if existing user, update (later)
+        const userExist = await User.findOne({auth0Id});
+        if (userExist) {
+            return res.status(200).json(userExist);
+        }
+
+        // if new user, add to db
+        // get user info from request body (will be passed by frontend)
+        const {name, email} = req.body;
+        const userData = new User({
+            auth0Id,
+            email,
+            name
+        });
+        await userData.save();
+        res.status(201).json(userData);
+        }
+
+    catch (error) {
+        console.error("Error in user sync:", error);
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
 });
 
 // routes
