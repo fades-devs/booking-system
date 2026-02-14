b6e76aa1a5c305de4a573e3f632794541b702aee
fades <fades.devs@gmail.com>
2026-02-14 16:35:44 +0000

    Created user model/schema for user service

---
+++ user-service/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['client', 'partner'],
+        default: 'client'
+    },
+    auth0Id: { // We get ID from login
+        type: String,
+        required: true,
+        unique: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema)
