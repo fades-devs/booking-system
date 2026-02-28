de0e741b80e8c071742a894d3dd7ae8e7b0ef4da
fades <fades.devs@gmail.com>
2026-02-28 12:05:17 +0000

    Refactored and cleaned up App.jsx file

--- frontend/vite-booking-system/src/App.jsx
+++ frontend/vite-booking-system/src/App.jsx
@@ -3,35 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './components/Profile';
+import {Route, Routes} from 'react-router-dom';
 
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-
-        <h1>Auth0 Login</h1>
-        <LoginButton />
-        <LogoutButton />
-        <Profile />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+        </Routes>
+      </main>
     </>
   )
 }
