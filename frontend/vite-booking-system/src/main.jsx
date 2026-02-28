8eb13a39a9f0998f945eea723ff6cb2c9b25cae3
fades <fades.devs@gmail.com>
2026-02-28 12:05:40 +0000

    Wrapped the app in browser router for routing

--- frontend/vite-booking-system/src/main.jsx
+++ frontend/vite-booking-system/src/main.jsx
@@ -4,21 +4,24 @@ import './index.css'
 import App from './App.jsx'
 
 import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Auth0Provider
-    domain={import.meta.env.VITE_AUTH0_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-    authorizationParams={
-      {
-        redirect_uri: window.location.origin,
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE //Probs not needed
+    <BrowserRouter basename="/booking-system/tree/main/frontend/vite-booking-system">
+      <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={
+        {
+          redirect_uri: window.location.origin,
+          audience: import.meta.env.VITE_AUTH0_AUDIENCE //Probs not needed
+        }
       }
-    }
-    >
-      <App />
-    </Auth0Provider>
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
