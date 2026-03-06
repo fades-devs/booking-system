3fe2c76bda63ed1c71ad86d4e654274a6463bcfd
fades <fades.devs@gmail.com>
2026-03-06 14:48:19 +0000

    Implemented logout after session inactivity  using idle hook and useIldeTimer function

--- frontend/vite-booking-system/src/App.jsx
+++ frontend/vite-booking-system/src/App.jsx
@@ -4,13 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import {Route, Routes} from 'react-router-dom';
+import {useIdleTimer} from 'react-idle-timer';
 
 import Home from './pages/Home';
 import MyBookings from './pages/MyBookings';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+
+  const {logout, isAuthenticated} = useAuth0();
+
+  const onIdle = () => {
+    if (isAuthenticated) {
+      alert('You have been logged out due to inactivity.');
+      logout({logoutParams: {returnTo: window.location.origin}})
+    }
+  }
+
+  useIdleTimer({
+    onIdle,
+    timeout: 1000 * 60 * 20, // 15 mins
+  })
 
   return (
     <div className='flex flex-col min-h-screen bg-slate-50 font-sans text-slate-900'>
