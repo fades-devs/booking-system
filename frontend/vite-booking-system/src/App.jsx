3b5a6bf634afc680e904f92f56cd2cdf544a9038
fades <fades.devs@gmail.com>
2026-03-06 11:18:52 +0000

    Styling of App.jsx using tailwind css

--- frontend/vite-booking-system/src/App.jsx
+++ frontend/vite-booking-system/src/App.jsx
@@ -7,22 +7,24 @@ import {Route, Routes} from 'react-router-dom';
 
 import Home from './pages/Home';
 import MyBookings from './pages/MyBookings';
-import Navbar from './components/client/Navbar';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 
   return (
-    <>
-    <header>
+    <div className='flex flex-col min-h-screen bg-slate-50 font-sans text-slate-900'>
+    <header className='bg-slate-900 text-white p-5'>
       <Navbar />
     </header>
-      <main>
+      <main className='flex flex-col grow w-full mx-auto p-5'>
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route path="/my-bookings" element={ <MyBookings />} />
+          <Route path="/dashboard" element={ <Dashboard /> }></Route>
         </Routes>
       </main>
-    </>
+    </div>
   )
 }
 
