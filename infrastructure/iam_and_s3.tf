29f1e33578843a7763e028801f3835a9ab1dbee3
fades <fades.devs@gmail.com>
2026-04-04 22:33:08 +0100

    Unblocked network access for pictures S3 bucket

--- infrastructure/iam_and_s3.tf
+++ infrastructure/iam_and_s3.tf
@@ -3,6 +3,33 @@ resource "aws_s3_bucket" "room_images" {
   bucket = "room-picture-s3-fades-2026"
 }
 
+# Turn off the public block
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.room_images.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Allow anyone on the internet to READ (GetObject) the images
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.room_images.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = ["s3:GetObject"]
+        Effect    = "Allow"
+        Principal = "*"
+        Resource  = "${aws_s3_bucket.room_images.arn}/*"
+      },
+    ]
+  })
+  # Ensure the block is removed before applying the policy
+  depends_on = [aws_s3_bucket_public_access_block.public_access] 
+}
+
 # 2. Standard ECS Execution Role (To pull Docker images)
 resource "aws_iam_role" "ecs_execution_role" {
   name = "ecsTaskExecutionRole-TF"
