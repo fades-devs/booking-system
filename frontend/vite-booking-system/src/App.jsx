cdf462394af93304826174b4ec8277d55085b9fe
fades <fades.devs@gmail.com>
2026-04-05 23:52:08 +0100

    Implemented adding user to database after authentication and routing in api gateway 2

--- frontend/vite-booking-system/src/App.jsx
+++ frontend/vite-booking-system/src/App.jsx
@@ -7,6 +7,7 @@ import MyBookings from "./pages/MyBookings";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import { useAuth0 } from "@auth0/auth0-react";
+import UserSync from "./components/UserSync";
 
 function App() {
   const { logout, isAuthenticated } = useAuth0();
@@ -27,6 +28,7 @@ function App() {
     <div className="flex flex-col min-h-screen bg-slate-50 font-sans text-slate-900">
       <header className="bg-slate-900 text-white p-5">
         <Navbar />
+        <UserSync />
       </header>
       <main className="flex flex-col grow w-full mx-auto p-5">
         <Toaster
